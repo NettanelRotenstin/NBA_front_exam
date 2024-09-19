174acc9139deb60b084f79960fb7c76aed7bb35e
you <nettanel1999@gmail.com>
2024-09-19 16:23:39 +0300

    continue

--- basketball.js
+++ basketball.js
@@ -16,6 +16,11 @@ const pgPick = document.querySelector(`.pg`);
 const sgPick = document.querySelector(`.sg`);
 const sfPick = document.querySelector(`.sf`);
 const pfPick = document.querySelector(`.pf`);
+const cPickDivs = document.querySelector(`.c div`);
+const pgPickDivs = document.querySelector(`.pg div`);
+const sgPickDivs = document.querySelector(`.sg div`);
+const sfPickDivs = document.querySelector(`.sf div`);
+const pfPickDivs = document.querySelector(`.pf div`);
 //selector table
 const tableSel = document.querySelector(`table`);
 //selectors inputs
@@ -26,7 +31,7 @@ const psnSel = document.querySelector(`.position`);
 //selector search button
 const btnSearch = document.querySelector(`.submit`);
 //base url
-const BASE_URL = `https://nbaserver-q21u.onrender.com/api/filter`;
+const BASE_URL = ` https://nbaserver-q21u.onrender.com/api/filter`;
 //arrey of my team
 const myTeam = [];
 //function return player object for api from input
@@ -42,23 +47,16 @@ const createPlayerObject = () => {
 //async function that called by post to server
 const callAPI = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        //const search:Player = await createPlayerObject()
+        const playerObject = createPlayerObject();
         const res = yield fetch(BASE_URL, {
-            method: `POST`,
+            method: "POST",
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({
-                position: psnSel.value,
-                twoPercent: +twoSel.value,
-                threePercent: +threeSel.value,
-                points: +pntSel.value
-            })
+            body: yield JSON.stringify(playerObject)
         });
-        if (res.ok) {
-            const relevantPlayers = yield res.json();
-            yield fillTheTable(relevantPlayers);
-        }
+        const relevantPlayers = yield res.json();
+        yield fillTheTable(relevantPlayers);
     }
     catch (err) {
         alert(`Couldn't proccess the search!`);
@@ -75,7 +73,7 @@ const fillTheTable = (arreyOfPlayers) => {
         const td3ToAdd = document.createElement(`td`);
         const tdAddToAdd = document.createElement(`td`);
         tdNmaeToAdd.textContent = arreyOfPlayers[i].playerName;
-        // i made the ! becouse i knoe ill get it from api
+        // i made the ! becouse i knoe i'll get it from api
         tdPsnToAdd.textContent = arreyOfPlayers[i].position;
         tdPtsToAdd.textContent = arreyOfPlayers[i].points.toString();
         td2ToAdd.textContent = arreyOfPlayers[i].twoPercent.toString();
@@ -83,7 +81,79 @@ const fillTheTable = (arreyOfPlayers) => {
         const btnAddToTeam = document.createElement(`button`);
         btnAddToTeam.classList.add(`btnAddToTeam`);
         tdAddToAdd.appendChild(btnAddToTeam);
-        tableSel.append(tdNmaeToAdd, tdPsnToAdd, tdPtsToAdd, td2ToAdd, td3ToAdd, tdAddToAdd);
+        trToAdd.append(tdNmaeToAdd, tdPsnToAdd, tdPtsToAdd, td2ToAdd, td3ToAdd, tdAddToAdd);
+        tableSel.appendChild(trToAdd);
+        btnAddToTeam.addEventListener(`click`, () => {
+            myTeam.filter(p => p.position !== arreyOfPlayers[i].position);
+            myTeam.push(arreyOfPlayers[i]);
+            fillMyTeam(myTeam);
+        });
     }
+};
+const fillMyTeam = (myTeam) => {
+    myTeam.forEach(P => {
+        switch (P.position) {
+            case `c`: {
+                cPickDivs.innerHTML = ``;
+                const divName = document.createElement(`div`);
+                const div3Per = document.createElement(`div`);
+                const div2Per = document.createElement(`div`);
+                const divPoints = document.createElement(`div`);
+                divName.textContent = `${P.playerName}`;
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`;
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`;
+                divPoints.innerText = `Points : ${P.points}`;
+                cPick.append(divName, div3Per, div2Per, divPoints);
+            }
+            case `pg`: {
+                pgPickDivs.innerHTML = ``;
+                const divName = document.createElement(`div`);
+                const div3Per = document.createElement(`div`);
+                const div2Per = document.createElement(`div`);
+                const divPoints = document.createElement(`div`);
+                divName.textContent = `${P.playerName}`;
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`;
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`;
+                divPoints.innerText = `Points : ${P.points}`;
+                pgPick.append(divName, div3Per, div2Per, divPoints);
+            }
+            case `sg`: {
+                sgPickDivs.innerHTML = ``;
+                const divName = document.createElement(`div`);
+                const div3Per = document.createElement(`div`);
+                const div2Per = document.createElement(`div`);
+                const divPoints = document.createElement(`div`);
+                divName.textContent = `${P.playerName}`;
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`;
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`;
+                divPoints.innerText = `Points : ${P.points}`;
+                sgPick.append(divName, div3Per, div2Per, divPoints);
+            }
+            case `sf`: {
+                sfPickDivs.innerHTML = ``;
+                const divName = document.createElement(`div`);
+                const div3Per = document.createElement(`div`);
+                const div2Per = document.createElement(`div`);
+                const divPoints = document.createElement(`div`);
+                divName.textContent = `${P.playerName}`;
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`;
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`;
+                divPoints.innerText = `Points : ${P.points}`;
+                sfPick.append(divName, div3Per, div2Per, divPoints);
+            }
+            case `pf`: {
+                pfPickDivs.innerHTML = ``;
+                const divName = document.createElement(`div`);
+                const div3Per = document.createElement(`div`);
+                const div2Per = document.createElement(`div`);
+                const divPoints = document.createElement(`div`);
+                divName.textContent = `${P.playerName}`;
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`;
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`;
+                divPoints.innerText = `Points : ${P.points}`;
+                pfPick.append(divName, div3Per, div2Per, divPoints);
+            }
+        }
+    });
 };
 btnSearch.addEventListener(`click`, callAPI);
--- basketball.ts
+++ basketball.ts
@@ -22,7 +22,15 @@ const sfPick: HTMLDivElement = document.querySelector(`.sf`)!
 
 const pfPick: HTMLDivElement = document.querySelector(`.pf`)!
 
+const cPickDivs: HTMLDivElement = document.querySelector(`.c div`)!
 
+const pgPickDivs: HTMLDivElement = document.querySelector(`.pg div`)!
+
+const sgPickDivs: HTMLDivElement = document.querySelector(`.sg div`)!
+
+const sfPickDivs: HTMLDivElement = document.querySelector(`.sf div`)!
+
+const pfPickDivs: HTMLDivElement = document.querySelector(`.pf div`)!
 //selector table
 
 const tableSel: HTMLTableElement = document.querySelector(`table`)!
@@ -40,10 +48,10 @@ const psnSel: HTMLInputElement = document.querySelector(`.position`)!
 //selector search button
 
 const btnSearch: HTMLInputElement = document.querySelector(`.submit`)!
- 
+
 //base url
 
-const BASE_URL: string = `https://nbaserver-q21u.onrender.com/api/filter`
+const BASE_URL: string = ` https://nbaserver-q21u.onrender.com/api/filter`
 
 //arrey of my team
 
@@ -65,37 +73,36 @@ const createPlayerObject = (): Player => {
     }
     return playerToSearch
 }
-
+ 
 
 
 
 //async function that called by post to server
 
 const callAPI = async (): Promise<void> => {
-        try {
-        //const search:Player = await createPlayerObject()
-         
+    try {
+
+        const playerObject = createPlayerObject()
+
         const res: Response = await fetch(BASE_URL, {
-            method: `POST`,
+            method: "POST",
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({
-                position: psnSel.value,
-                twoPercent: +twoSel.value,
-                threePercent: +threeSel.value,
-                points: +pntSel.value
-        })
+            body: await JSON.stringify(
+                playerObject
+
+            )
 
         })
-        
-        if (res.ok) {
-            const relevantPlayers: Player[] = await res.json()
-             
-            await fillTheTable(relevantPlayers)
-        }
+
+        const relevantPlayers: Player[] = await res.json()
+
+        await fillTheTable(relevantPlayers)
+
     }
     catch (err) {
+
         alert(`Couldn't proccess the search!`)
     }
 }
@@ -103,49 +110,185 @@ const callAPI = async (): Promise<void> => {
 
 //function thats fill the table
 
-const fillTheTable = (arreyOfPlayers:Player[]):void =>{
-    
+const fillTheTable = (arreyOfPlayers: Player[]): void => {
+
     for (let i = 0; i < arreyOfPlayers.length; i++) {
-       
-        const trToAdd:HTMLTableRowElement = document.createElement(`tr`)  
 
-        const tdNmaeToAdd:HTMLElement = document.createElement(`td`)
+        const trToAdd: HTMLTableRowElement = document.createElement(`tr`)
 
-        const tdPsnToAdd:HTMLElement = document.createElement(`td`)
+        const tdNmaeToAdd: HTMLElement = document.createElement(`td`)
 
-        const tdPtsToAdd:HTMLElement = document.createElement(`td`)
+        const tdPsnToAdd: HTMLElement = document.createElement(`td`)
 
-        const td2ToAdd:HTMLElement = document.createElement(`td`)
+        const tdPtsToAdd: HTMLElement = document.createElement(`td`)
 
-        const td3ToAdd:HTMLElement = document.createElement(`td`)
+        const td2ToAdd: HTMLElement = document.createElement(`td`)
 
-        const tdAddToAdd:HTMLElement = document.createElement(`td`)
+        const td3ToAdd: HTMLElement = document.createElement(`td`)
 
-         
-        
+        const tdAddToAdd: HTMLElement = document.createElement(`td`)
+
+
         tdNmaeToAdd.textContent = arreyOfPlayers[i].playerName!
 
-        // i made the ! becouse i knoe ill get it from api
+        // i made the ! becouse i knoe i'll get it from api
 
         tdPsnToAdd.textContent = arreyOfPlayers[i].position
 
-        tdPtsToAdd.textContent = arreyOfPlayers[i].points.toString()        
+        tdPtsToAdd.textContent = arreyOfPlayers[i].points.toString()
 
         td2ToAdd.textContent = arreyOfPlayers[i].twoPercent.toString()
 
         td3ToAdd.textContent = arreyOfPlayers[i].threePercent.toString()
 
-        const btnAddToTeam:HTMLButtonElement = document.createElement(`button`)
+        const btnAddToTeam: HTMLButtonElement = document.createElement(`button`)
 
         btnAddToTeam.classList.add(`btnAddToTeam`)
 
         tdAddToAdd.appendChild(btnAddToTeam)
 
-        tableSel.append(tdNmaeToAdd,tdPsnToAdd,tdPtsToAdd,td2ToAdd,td3ToAdd,tdAddToAdd)
+        trToAdd.append(tdNmaeToAdd, tdPsnToAdd, tdPtsToAdd, td2ToAdd, td3ToAdd, tdAddToAdd)
+
+        tableSel.appendChild(trToAdd)
+
+        btnAddToTeam.addEventListener(`click`, (): void => {
+            myTeam.filter(p => p.position !== arreyOfPlayers[i].position)
+            myTeam.push(arreyOfPlayers[i])
+             
+            fillMyTeam(myTeam)
+
+        })
 
     }
 }
 
 
 
-btnSearch.addEventListener(`click`,callAPI)
+const fillMyTeam = (myTeam: Player[]): void => {
+    myTeam.forEach(P => {
+        switch(P.position){
+            case `c`:{
+                cPickDivs.innerHTML = ``
+
+                const divName:HTMLDivElement = document.createElement(`div`)!
+
+                const div3Per:HTMLDivElement = document.createElement(`div`)!
+
+                const div2Per:HTMLDivElement = document.createElement(`div`)!
+
+                const divPoints:HTMLDivElement = document.createElement(`div`)!
+
+                divName.textContent = `${P.playerName}`
+
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`
+
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`
+
+                divPoints.innerText = `Points : ${P.points}`
+
+                cPick.append(divName,div3Per,div2Per,divPoints)
+
+            }
+            case `pg`:{
+                pgPickDivs.innerHTML = ``
+
+                const divName:HTMLDivElement = document.createElement(`div`)!
+
+                const div3Per:HTMLDivElement = document.createElement(`div`)!
+
+                const div2Per:HTMLDivElement = document.createElement(`div`)!
+
+                const divPoints:HTMLDivElement = document.createElement(`div`)!
+
+                divName.textContent = `${P.playerName}`
+
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`
+
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`
+
+                divPoints.innerText = `Points : ${P.points}`
+
+                pgPick.append(divName,div3Per,div2Per,divPoints)
+
+            }
+
+            case `sg`:{
+                sgPickDivs.innerHTML = ``
+
+                const divName:HTMLDivElement = document.createElement(`div`)!
+
+                const div3Per:HTMLDivElement = document.createElement(`div`)!
+
+                const div2Per:HTMLDivElement = document.createElement(`div`)!
+
+                const divPoints:HTMLDivElement = document.createElement(`div`)!
+
+                divName.textContent = `${P.playerName}`
+
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`
+
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`
+
+                divPoints.innerText = `Points : ${P.points}`
+
+                sgPick.append(divName,div3Per,div2Per,divPoints)
+
+            }
+            case `sf`:{
+                sfPickDivs.innerHTML = ``
+
+                const divName:HTMLDivElement = document.createElement(`div`)!
+
+                const div3Per:HTMLDivElement = document.createElement(`div`)!
+
+                const div2Per:HTMLDivElement = document.createElement(`div`)!
+
+                const divPoints:HTMLDivElement = document.createElement(`div`)!
+
+                divName.textContent = `${P.playerName}`
+
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`
+
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`
+
+                divPoints.innerText = `Points : ${P.points}`
+
+                sfPick.append(divName,div3Per,div2Per,divPoints)
+
+            }
+
+            case `pf`:{
+                pfPickDivs.innerHTML = ``
+
+                const divName:HTMLDivElement = document.createElement(`div`)!
+
+                const div3Per:HTMLDivElement = document.createElement(`div`)!
+
+                const div2Per:HTMLDivElement = document.createElement(`div`)!
+
+                const divPoints:HTMLDivElement = document.createElement(`div`)!
+
+                divName.textContent = `${P.playerName}`
+
+                div3Per.innerText = `Three Percent : ${P.threePercent}%`
+
+                div2Per.innerHTML = `Two Percent : ${P.twoPercent}`
+
+                divPoints.innerText = `Points : ${P.points}`
+
+                pfPick.append(divName,div3Per,div2Per,divPoints)
+
+            }
+
+
+        }
+    });
+}
+
+ 
+
+
+
+btnSearch.addEventListener(`click`, callAPI)
+
+
